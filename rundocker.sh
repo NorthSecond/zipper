ee30803a994dab0d6ee69ae43c860412ecdfb30e
yangyf83 <yangyf83@aliyun.com>
2023-01-16 21:29:08 +0800

    update docker

--- rundocker.sh
+++ rundocker.sh
@@ -1,7 +1,7 @@
 docker build -t zipper .
 
-docker run --rm -it -v ~/vscode-extension/.vscode-server:/root/.vscode-server -v ~/code/zipper:/home/project zipper:v0
-
-docker run --rm -it -d -v ~/vscode-extension/.vscode-server:/root/.vscode-server -v ~/code/zipper:/home/project zipper:0.1.0
+#docker run --rm -it -v ~/vscode-extension/.vscode-server:/root/.vscode-server -v ~/code/zipper:/home/project zipper:v0
+#
+#docker run --rm -it -d -v ~/vscode-extension/.vscode-server:/root/.vscode-server -v ~/code/zipper:/home/project zipper:0.1.0
 
 docker run --rm -it -d -v ~/code/zipper:/home/project zipper:0.1.0
